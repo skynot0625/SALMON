277e660720c429b0e4aea38a82b8c814480cba17
Siguros <duddns157@snu.ac.kr>
2023-11-29 15:45:00 +0900

    11.29

--- SALMON-main/src/train.py
+++ SALMON-main/src/train.py
@@ -8,6 +8,10 @@ from lightning import Callback, LightningDataModule, LightningModule, Trainer
 from lightning.pytorch.loggers import Logger
 from omegaconf import DictConfig
 
+# aihwkit module import
+from src.analog_lenet import AnalogLeNet
+
+
 rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
 # ------------------------------------------------------------------------------------ #
 # the setup_root above is equivalent to:
